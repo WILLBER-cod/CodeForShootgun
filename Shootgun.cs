16f2b89e3b65e490f8796bad525ab68b8bda2819
WILLBER-cod <66934794+WILLBER-cod@users.noreply.github.com>
2020-10-28 16:00:41 +0800

    Create Shootgun.cs

---
+++ Shootgun.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shootgun : Weapon
+{
+    [SerializeField] private Bullet[] _bullet;
+
+    public override void Shoot(Transform shootPoint)
+    {
+        for (int i = 0; i < _bullet.Length; i++)
+        {
+            Instantiate(_bullet[i], shootPoint.position, _bullet[i].transform.rotation);
+        }
+    }
+}
